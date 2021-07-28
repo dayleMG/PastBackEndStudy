41f8d1076370837afb3d20a3a3ba03b81955b0ef
dayle998 <dayle998@naver.com>
2021-07-28 17:00:52 +0900

    Reafactor: Adapter 패턴 적용
    Adapter 패턴을 통해 ControllerV3, V4을 동시 사용 가능
    -> 다양한 종류의 컨트롤러 사용 가능

---
+++ src/main/java/study/servlet/web/frontcontroller/v5/FrontControllerServlet5.java
@@ -0,0 +1,85 @@
+package study.servlet.web.frontcontroller.v5;
+
+import study.servlet.web.frontcontroller.ModelView;
+import study.servlet.web.frontcontroller.MyView;
+import study.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import study.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import study.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import study.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import study.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import study.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+import study.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import study.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServlet5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServlet5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServlet5() {
+        initHandlerMappingMap();
+        initHandlerAdapters();
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
+    }
+
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String requestURI = req.getRequestURI();
+        Object handler = handlerMappingMap.get(requestURI);
+
+        if(handler == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyHandlerAdapter adapter = getHandlerAdapter(handler);
+
+        ModelView mv = adapter.handle(req, resp, handler);
+
+        String viewName = mv.getViewName();
+        MyView view = viewResolver(viewName);
+
+        view.render(mv.getModel(), req, resp);
+
+
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter adapter : handlerAdapters) {
+            if(adapter.supports(handler)) {
+                return adapter;
+            }
+        }
+        throw new IllegalArgumentException("handler adapter를 찾을 수 없습니다. handler=" + handler);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+}
